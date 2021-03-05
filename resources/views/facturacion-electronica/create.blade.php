3a2a550a7ef844b5888e3652cfdb16894fba1270
lucianojs110 <lucianojs110@gmail.com>
2021-03-05 20:48:51 -0300

    ajax arreglado

--- resources/views/facturacion-electronica/create.blade.php
+++ resources/views/facturacion-electronica/create.blade.php
@@ -4,22 +4,25 @@
 
 <section class="content">
 
-    <h3>Nueva Factura Electronica</h3>
-    <h4>Seleccione periodo a facturar</h4>
+    <h3>Nueva Factura Electrónica</h3>
+    <h4>Seleccione período a facturar</h4>
 
 
     <div class="row">
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-2">
             <label>Año</label>
             <select name="year" id="year" class="form-control">
-                <option value="0">Año</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
-                <option value="2023">2023</option>
-
+            <option value="0">Año</option>
+            <?php  
+                 $year = date("Y");
+                 $yeari = $year-2;
+                 $yearf = $year+2;
+                 for($i=$yeari;$i<=$yearf;$i++) 
+                    { echo "<option value='".$i."'>".$i."</option>"; } 
+             ?>
             </select>
         </div>
-        <div class="form-group col-md-3">
+        <div class="form-group col-md-2">
             <label>Mes</label>
             <select name="mes" id="mes" class="form-control">
                 <option value="0">Mes</option>
@@ -37,8 +40,7 @@
                 <option value="12">Diciembre</option>
             </select>
         </div>
-        <div class="form-group col-md-3">
-            <br>
+        <div class="form-group col-md-3" style="margin-top: 25px">
             <button class="btn btn-primary" id="consultar" type="button"> Consultar</button>
         </div>
     </div>
@@ -48,24 +50,28 @@
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
             <table class="table table-striped table-bordered table-condensed table-hover">
                 <thead style="background-color: #A9D0F5">
-                    <th style="font-size: 15px; text-align:left">Cant.</th>
-                    <th style="font-size: 15px; text-align:left">Prestación.</th>
-                    <th style="font-size: 15px; text-align:left">Valor Modulo</th>
-                    <th style="font-size: 15px; text-align:left">Sub Total</th>
+                    <th>Cant.</th>
+                    <th>Prestación</th>
+                    <th>Valor Modulo</th>
+                    <th>Sub Total</th>
                 </thead>
                 <tbody id="bodytablafac">
                 </tbody>
-                <tbody>
-                    <th>TOTAL</th>
-             
-                   
+                <tfoot>
                     <th></th>
                     <th></th>
-                    <th>
-                        <h4 id="total">$ 0.00</h4><input type="hidden" name="total_venta" id="total_venta">
+                    <th style="font-size: 18px; text-align:right">TOTAL:</th>
+                    <th style="font-size: 18px; text-align:left">
+                        <span id="total" >$ 0.00</span>
                     </th>
-                </tbody>
+                </tfoot>
             </table>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+        <button class="btn btn-primary" id="consultar" type="button"> Facturar</button>
         </div>
     </div>
 
@@ -100,6 +106,7 @@
                     
                     if(data.length==0){
                         $("#bodytablafac").html("<h3>No hay items a facturar en este periodo</h3>");
+                        $('#total').html('$0.00');
                     }else {
                     
                     for (i = 0; i < data.length; i++) { //cuenta la cantidad de registros
@@ -108,14 +115,14 @@
                             data[i].cantidad + "</td><td>" +
                             data[i].nombre_pres + "</td><td>" +
                             data[i].valor_modulo + "</td><td>" +
-                            ((parseFloat(data[i].cantidad ).toFixed(2)) * (parseFloat(data[i].valor_modulo).toFixed(2) )) + "</td><td>"
+                            ((parseFloat(data[i].cantidad ).toFixed(2)) * (parseFloat(data[i].valor_modulo).toFixed(2) )) + "</td></tr>"
                         $("#bodytablafac").append(nuevafila)
                          
                          total = total + (parseFloat(data[i].cantidad ).toFixed(2)) * (parseFloat(data[i].valor_modulo).toFixed(2) );
                         
                     }
 
-                    $('#total').html(total);
+                    $('#total').html('$'+total);
                        }
                 },
                 error: function () {
